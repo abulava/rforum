8101830babcb0d27e721f328c6d329ebc752a435
Andrei Bulava <abulava@gmail.com>
2012-02-07 12:10:10 +0200

    Cleaning up after Paperclip Cucumber tests.

--- features/support/custom_env.rb
+++ features/support/custom_env.rb
@@ -1,1 +1,14 @@
 require "factory_girl/step_definitions"
+require 'paperclip'
+
+module Paperclip::Interpolations 
+  alias_method :orig_attachment, :attachment 
+  def attachment(att, style) 
+    File.join('cucumber', orig_attachment(att, style))
+  end 
+end
+
+After do 
+  test_dir = File.join(Rails.root.to_s, 'public', 'system', 'cucumber')
+  `rm -rf #{test_dir}`  #remove paperclip files
+end
